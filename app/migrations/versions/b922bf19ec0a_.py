893f24f6dc7fe577c68e4c26c4952ae7a52aa78a
addirossi <addirossi@gmail.com>
2022-07-26 03:21:04 +0600

    Add schema

---
+++ app/migrations/versions/b922bf19ec0a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b922bf19ec0a
+Revises: fa35bd9ab4ae
+Create Date: 2022-07-26 03:16:25.299218
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b922bf19ec0a'
+down_revision = 'fa35bd9ab4ae'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
